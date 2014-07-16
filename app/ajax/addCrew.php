4fbe7ff84e1bd49d5cf13894726744d351569ea3
Liu, Raymond P. (ARC-AFO)[UNIV OF CALIFORNIA   SANTA CRUZ] <raymond.p.liu@nasa.gov>
2014-07-15 17:08:18 -0700

    Renamed addCrew scripts...

--- app/ajax/addCrew.php
+++ app/ajax/addCrew.php
@@ -1,13 +1,72 @@
 <?php 
 	require_once 'db.php'; // The mysql database connection script
-	$id=$_REQUEST['id'];
+	$id="'".stripslashes($_REQUEST['id'])."'";
 	$time=$_REQUEST['time'];
 	$lat=$_REQUEST['latitude'];
 	$lon=$_REQUEST['longitude']; //
-	$route=$_REQUEST['route'];
-	$eta=$_REQUEST['eta'];
+	$destlat=$_REQUEST['destlat'];
+	$destlong=$_REQUEST['destlong'];
 
-	$query="INSERT INTO androidnavtable (id, timeSecond, latitudeDegree, longitudeDegree, route, eta) VALUES(".$id.",".$time.",".$lat.",".$lon.",".$route.",".$eta.")";
+	$url = "http://maps.googleapis.com/maps/api/directions/json?origin="  . $lat . "," . $lon . "&destination=" . $destlat . "," . $destlong . "&sensor=false&units=metric&mode=driving";
+
+	//print($url); //URL working
+
+	$jsondata = file_get_contents($url); //this is working too, I think...
+	//print_r($jsondata);
+	$routes = json_decode($jsondata);
+	//print_r($routes);
+	$encoded = (string) $routes->routes[0]->overview_polyline->points;
+	//print($encoded); //OMG I GOT THE STRING
+	
+
+	$polyline = "";
+	$len = strlen($encoded);
+	$index = 0;
+	$first = true;
+
+	while ($index < $len) {
+        $b = 0; 
+        $shift = 0;
+        $result = 0;
+        do {
+            $b = $encoded[$index++] - 63;
+            $result |= ($b & 0x1f) << $shift;
+            $shift += 5;
+        } while ($b >= 0x20);
+        $dlat = (($result & 1) != 0 ? ~($result >> 1) : ($result >> 1));
+        $lat += $dlat;
+        $shift = 0;
+        $result = 0;
+        do {
+            $b = $encoded[$index++] - 63;
+            $result |= ($b & 0x1f) << $shift;
+            $shift += 5;
+        } while ($b >= 0x20);
+        $dlng = (($result & 1) != 0 ? ~($result >> 1) : ($result >> 1));
+        $lon += $dlng;
+
+       	$plat = $lat / 1E5;
+       	$plong = $lng / 1E5;
+
+       	if(!$first) {
+       		$polyline = $polyline . "|";
+       	}
+
+       	$polyline = $polyline . $lat . "," . $lon;
+       	$first = false;
+    }
+
+    $polyline = "'" . $polyline . "'"; //add quotes around polyline
+
+    //print($polyline); //I THINK THIS WORKS!!!
+
+    $eta=(string)$routes->routes[0]->legs[0]->duration->value;
+    //print_r($eta);
+    
+    
+    $query="INSERT INTO androidnavtable (id, timeSecond, latitudeDegree, longitudeDegree, route, eta) VALUES(".$id.",".$time.",".$lat.",".$lon.",".$polyline.",".$eta.")";
+
+    //print($query); check this if something breaks
 
 	$q=mysql_query($query);
 	if (!$q) {
@@ -21,15 +80,5 @@
 
 	print(json_encode($output));
 	mysql_close();
-
-	/*
-	if(isset($_GET['task'])){
-		$task = $_GET['task'];
-		$status = "0";
-		$created = time();
-		$query=mysql_query("INSERT INTO tasks(task,status,created_at)  VALUES ('$task', '$status', '$created')") or die(mysql_error());
-	}
-?>
-*/
 
 ?>
