0616ccab28c671310ad8bd6e574be90b362bb2ac
Agafonova, Olga I. (ARC-AFO)[UNIVERSITIES SPACE RESEARCH ASSOCIATION] <olga.agafonova@nasa.gov>
2014-07-24 11:45:47 -0700

    Renamed the site and added jasmine unit testing dependencies.

---
+++ app/spec/MapControllerHelper.js
@@ -0,0 +1,23 @@
+/*Sources: https://docs.angularjs.org/api/ngMock/service/$httpBackend
+https://github.com/markcoleman/angular-unittest/blob/b37249a70bd0b67363746e50eb161c42f628728d/test/SimpleServiceSpecs.js
+*/
+describe("mapController", function() 
+{
+    var scope, ctrl, $httpBackend;
+    beforeEach(module('FlightCrewApp'));
+
+    var coords,$httpBackend;
+
+    beforeEach(inject(function(_$httpBackend_){
+        $httpBackend = _$httpBackend_;
+    }));
+
+    describe("initAirportMarkers", function(){
+    it("should make an http call to data/airportpositions.json", function(){
+        //$httpBackend.whenGET("data/airportpositions.json").respond(coords);
+        //expect(coords).toBeDefined();
+        console.log("jasmine coords:" + coords);
+    })
+    });
+
+});
